3617e0fcd3a7873b1e605c118d2fc074ff75a8d9
alejandro <acervantes@interware.com.mx>
2016-04-01 16:48:53 -0600

    se agrega carpeta javasource y documento java

---
+++ javasource/World.java
@@ -0,0 +1,6 @@
+public class World {
+   public static void main (String[] args){
+     System.out.println("Welcome to GIT and Oracle Java 8");
+	System.out.println("This a new command line doc commited with git");
+   }
+}
