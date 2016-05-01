d044440ab941b36bd5ba80174142356dfd2c5a0b
alejandro cervantes <alexandrc68@gmail.com>
2016-05-01 17:41:43 -0500

    second commit from console

--- javasource/World.java
+++ javasource/World.java
@@ -2,6 +2,8 @@ public class World {
    public static void main (String[] args){
      System.out.println("Welcome to GIT and Oracle Java 8");
 	 System.out.println("This a new command line doc commited with git");
-	 System.out.println("New line added to test git...")
+	 System.out.println("New line added to test git...");
+	 System.out.println("Second line added to test git...");
+
    }
 }
