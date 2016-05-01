ff6362e0b39a1d7b745e239c00609732cbf0e878
alejandro cervantes <alexandrc68@gmail.com>
2016-05-01 17:33:28 -0500

    Line added from git command line

--- javasource/World.java
+++ javasource/World.java
@@ -1,6 +1,7 @@
 public class World {
    public static void main (String[] args){
      System.out.println("Welcome to GIT and Oracle Java 8");
-	System.out.println("This a new command line doc commited with git");
+	 System.out.println("This a new command line doc commited with git");
+	 System.out.println("New line added to test git...")
    }
 }
